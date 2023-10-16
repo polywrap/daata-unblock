7c7f7a22f0ef48c379f5fd054995ba3890af07ec
Pileks <pilex13@gmail.com>
2023-10-16 17:20:44 +0200

    add basic user icon

--- src/app/components/UserBox.tsx
+++ src/app/components/UserBox.tsx
@@ -1,5 +1,6 @@
-import { useUserStore } from "@/stores/userStore";
 import { useEffect, useState } from "react";
+
+import { useUserStore } from "@/stores/userStore";
 
 interface UserBoxProps {}
 
@@ -15,7 +16,22 @@ const UserBox = (props: UserBoxProps) => {
 
   return (
     <div className="p-0.5 flex items-center gap-2 pt-6">
-      <div className="w-8 h-8 rounded-full bg-white border-primary-100 border-2" />
+      <div className="w-8 h-8 rounded-full bg-white border-primary-100 border-2 flex justify-center items-center text-primary-300">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+          />
+        </svg>
+      </div>
       <p className="text-xs">{userDisplayName}</p>
     </div>
   );
